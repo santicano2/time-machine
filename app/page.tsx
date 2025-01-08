cfab367796126cf9a8b61366447e57cb109e0827
Santi <canosantiago404@gmail.com>
2025-01-08 10:33:36 -0300

    feat: added initial form

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import TimeMachineForm from "@/components/time-machine-form";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center p-24">
+    <div className="flex flex-col items-center justify-center p-24 gap-10">
       <h1 className="text-4xl font-bold">Time Machine</h1>
+      <TimeMachineForm />
     </div>
   );
 }
---
+++ components/time-machine-form.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  date: z.string().min(2).max(50),
+  latitude: z.number(),
+  longitude: z.number(),
+});
+
+export default function TimeMachineForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      date: "",
+      latitude: 0,
+      longitude: 0,
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-10"
+      >
+        <FormField
+          control={form.control}
+          name="date"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Año al que querés viajar</FormLabel>
+              <FormControl>
+                <Input placeholder="2002" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <div className="flex gap-4">
+          <FormField
+            control={form.control}
+            name="latitude"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Latitud</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="40.7128"
+                    {...field}
+                    onChange={(e) => field.onChange(Number(e.target.value))}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="longitude"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Longitud</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="20.15316"
+                    {...field}
+                    onChange={(e) => field.onChange(Number(e.target.value))}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button type="submit">Viajar en el tiempo</Button>
+      </form>
+    </Form>
+  );
+}
